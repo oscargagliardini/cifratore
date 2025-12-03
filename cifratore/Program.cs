d0d57d2efac0a971a7bff18463a660bdf80bdb0b
gagliardini.oscar <gagliardini.oscar@ACER-4048>
2025-12-03 13:34:45 +0100

    Aggiungere i file di progetto.

---
+++ cifratore/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace cifratore
+{
+    internal class Program
+    {
+        static char cesare(char[]p,int k, char[] v)
+        {
+
+            for(int i = 0;i < v.Length; i++)
+            {
+                int f = 0;
+                bool t= false;
+                while (t=false)
+                {
+
+                }
+
+            }
+
+            
+        }
+        static void Main(string[] args)
+        {
+            int k = 4;
+            Console.WriteLine("Hello, World!");
+            char[] alfabeto = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o' ,'p','q','r','s','t','u','v','w','x','y','z'};
+            char[] v = { 'c', 'i', 'a', 'o' };
+
+        }
+    }
+}
