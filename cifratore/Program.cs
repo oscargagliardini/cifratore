d88de4113e522ec808a57b147ed716752add5a3d
gagliardini.oscar <gagliardini.oscar@ACER-4051>
2025-12-04 10:48:49 +0100

    mdi

--- cifratore/Program.cs
+++ cifratore/Program.cs
@@ -2,29 +2,63 @@
 {
     internal class Program
     {
-        static char cesare(char[]p,int k, char[] v)
+        static char [] cesare(char[]alf,int k, char[] v)
         {
 
             for(int i = 0;i < v.Length; i++)
             {
-                int f = 0;
-                bool t= false;
-                while (t=false)
+                int rest = 0;
+                int j = 0;
+                bool t= true;
+                while (t==true)
                 {
-
+                    if (v[i] == alf[j])
+                    {
+                        if (j + k > alf.Length)
+                        {
+                            rest = j + k - alf.Length;
+                            v[i] = alf[rest];
+                        }
+                        else
+                        {
+                            v[i] = alf[j + k];
+                            
+                        }
+                        t = false;
+                    }
+                    j++;
                 }
-
+                t = true;
             }
-
+            return v;
             
+        }
+        static char[] transposizione(char[]v,int k)
+        {
+            char[]v2 = new char[v.Length];
+            for(int n = 0; n < v.Length; n++)
+            {
+                v2[n] = v[(n+k )% v.Length];
+            }
+            return v2;
         }
         static void Main(string[] args)
         {
-            int k = 4;
-            Console.WriteLine("Hello, World!");
+            int y = 2;
+            int k = 6;
             char[] alfabeto = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o' ,'p','q','r','s','t','u','v','w','x','y','z'};
-            char[] v = { 'c', 'i', 'a', 'o' };
-
+            char[] v = { 'v', 'a', 'l', 'e' };
+            char[] risultato = cesare(alfabeto, k, v);
+            for (int i = 0; i < risultato.Length; i++)
+            {
+                Console.Write(risultato[i]);
+            }
+            Console.WriteLine();
+           char [] risultato1=transposizione(risultato, k);
+            for(int s=0;s<risultato1.Length; s++)
+            {
+                Console.Write(risultato1[s]);
+            }
         }
     }
 }
